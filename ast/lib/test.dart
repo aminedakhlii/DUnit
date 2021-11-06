876ff2ff9ae3e16ac73408645661c67603f1fb37
aminedakhlii <amine.dakhli@medtech.tn>
2021-11-06 19:37:30 -0400

    adding test.dart

---
+++ ast/lib/test.dart
@@ -0,0 +1,35 @@
+class ExampleClass1 {
+  int field;
+
+  ExampleClass1({this.field}); 
+
+  method(param) {
+    return param; 
+  }
+
+  int getField(){
+    return field; 
+  }
+
+  void setLasmer(int field){
+    this.field = field; 
+  }
+}
+
+class ExampleClass2 {
+  int field2;
+
+  ExampleClass2({this.field2}); 
+
+  method(param) {
+    return param; 
+  }
+
+  int getField(){
+    return field2; 
+  }
+
+  void setLasmer(int field){
+    this.field2 = field; 
+  }
+}
